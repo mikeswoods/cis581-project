5d0c706b463a05cf142068c44c30b8487e8aa9ed
Mike Woods <mikeswoods@gmail.com>
2014-12-07 15:46:58 -0500

    Added individual feature detectors for bounding boxes

--- get_face_features.m
+++ get_face_features.m
@@ -1,54 +1,186 @@
-%% Given a face image, this function returns one or more struct instances
-% for each "real" face detected. Each struct has the following fields:
+%% 
 %
-% 'Face': face bounding box
-% <feature>: feature bounding box
+function [Z] = get_face_features(faceImage)
+    
+    % Do some initial preprocessing of the image
+    %I = imsharpen(imadjust(rgb2gray(faceImage)));
+    %I = imadjust(rgb2gray(faceImage));
+    I = faceImage;
+    
+    % First, try to find the nose the initial landmarks:
+    nose             = get_single_feature(I, 'Nose');
+    [nose_x, nose_y] = bbox_centroid_wh(nose);
+    nose_centroid    = [nose_x, nose_y];
+    
+    % Find all mouth candidates:
+    mouth              = get_any_feature(I, 'Mouth');
+    [mouth_x, mouth_y] = bbox_centroid_wh(mouth);
+    mouth = prune_mouth_candidates(nose_centroid, mouth, [mouth_x, mouth_y]);
+    
+    % Find all left eye candidates:
+    left_eye = get_any_feature(I, 'LeftEye');
+    if size(left_eye, 1) > 0
+        [left_eye_x, left_eye_y] = bbox_centroid_wh(left_eye);
+        left_eye = prune_left_eye_candidates(nose_centroid, left_eye, [left_eye_x, left_eye_y]);
+    end
+        
+    % Find all right eye candidates:
+    right_eye  = get_any_feature(I, 'RightEye');
+    if size(left_eye, 1) > 0
+        [right_eye_x, right_eye_y] = bbox_centroid_wh(right_eye);
+        right_eye = prune_right_eye_candidates(nose_centroid, right_eye, [right_eye_x, right_eye_y]);
+    end
+
+    %%%
+    
+    imshow(I);
+    hold on;
+    rectangle('Position', nose, 'EdgeColor', 'r', 'LineWidth', 2);
+    plot(nose_x, nose_y, 'o', 'Color', 'g');
+    
+    for i=1:size(mouth, 1)
+        rectangle('Position', mouth(i,:), 'EdgeColor', 'b', 'LineWidth', 2);
+    end
+    plot(mouth_x, mouth_y, 'o', 'Color', 'g');
+
+    N = size(left_eye, 1);
+    for i=1:N
+        rectangle('Position', left_eye(i,:), 'EdgeColor', 'g', 'LineWidth', 2);
+    end
+    if N > 0
+        plot(left_eye_x, left_eye_y, 'o', 'Color', 'g');
+    end
+    
+    N = size(right_eye, 1);
+    for i=1:size(right_eye, 1)
+        rectangle('Position', right_eye(i,:), 'EdgeColor', 'y', 'LineWidth', 2);
+    end
+    if N > 0
+        plot(right_eye_x, right_eye_y, 'o', 'Color', 'g');
+    end
+
+    hold off;
+end
+
+%%
 %
-function [bboxStructs] = get_face_features(faceImage, feature)
+function [mouthsPassing] = prune_mouth_candidates(nose_centroid, mouths, mouth_centroid)
+    if size(mouths, 1) == 1
+        mouthsPassing = mouths;
+    else
+        % Only keep mouths with a y coordinate greater than that of the nose:
+        % This indicates that the mouth should be below the nose.
+        % Note this assumes the face is in an upright position!!!
+        mouthsPassing = mouths(find(mouth_centroid(:,2) > nose_centroid(:,2)),:);
+    end
+end
 
-    % Do some sharpening preprocessing:
-    faceImage = imsharpen(faceImage);
+%%
+%
+function [eyesPassing] = prune_left_eye_candidates(nose_centroid, eyes, eyes_centroid)
+    if size(eyes, 1) == 1
+        eyesPassing = eyes;
+    else
+        % Only keep eyes with a y coordinate less than that of the nose
+        % This indicates that the eyes should be above the nose.
+        % Note this assumes the face is in an upright position!!!
+        INDEX         = find(eyes_centroid(:,2) < nose_centroid(:,2));
+        eyes          = eyes(INDEX,:);
+        eyes_centroid = eyes_centroid(INDEX,:);
+        
+        % Now, pick the eye with the x cooridate to the left of the nose
+        % nose centroid
+        eyesPassing = eyes(find(eyes_centroid(:,1) < nose_centroid(:,1)),:);
+    end
+end
 
-    faceDetector = vision.CascadeObjectDetector('FrontalFaceCART', 'MergeThreshold', 2);
-    faces        = faceDetector.step(faceImage);
-    bboxStructs  = [];
+%%
+%
+function [eyesPassing] = prune_right_eye_candidates(nose_centroid, eyes, eyes_centroid)
+    if size(eyes, 1) == 1
+        eyesPassing = eyes;
+    else
+        % Only keep eyes with a y coordinate less than that of the nose
+        % This indicates that the eyes should be above the nose.
+        % Note this assumes the face is in an upright position!!!
+        INDEX         = find(eyes_centroid(:,2) < nose_centroid(:,2));
+        eyes          = eyes(INDEX, :);
+        eyes_centroid = eyes_centroid(INDEX, :);
+        
+        % Now, pick the eye with the x cooridate to the left of the nose
+        % nose centroid
+        eyesPassing = eyes(find(eyes_centroid(:,1) > nose_centroid(:,1)),:);
+    end
+end
+
+%%
+%
+function [x,y] = bbox_centroid_wh(bbox)
+
+    x1 = bbox(:,1);
+    y1 = bbox(:,2);
+    x2 = x1 + bbox(:,3);
+    y2 = y1 + bbox(:,4);
+    x = (x1 + x2) ./ 2;
+    y = (y1 + y2) ./ 2;
+end
+
+%%
+%
+function [x,y] = bbox_centroid_xy(bbox)
+    x = (bbox(:,1) + bbox(:,3)) ./ 2;
+    y = (bbox(:,2) + bbox(:,4)) ./ 2;
+end
+
+%%
+%
+function [BBOX] = get_any_feature(I, feature_type, hi, lo)
+
+    BBOX = [];
+
+    if nargin ~= 4
+        lo = 3;
+    end
+    if nargin ~= 3
+        hi = 15;
+    end
     
-    numFaces = size(faces,1);
-    fprintf(1, 'get_face_features: Found %d face(s)\n', numFaces);
-    
-    for i=1:numFaces
- 
-        foundFeature = false;
- 
-        % Try different thresholds for finding the feature:
-        for t=14:-1:2
-            
-            fprintf('-- face: %d/%d @ feature threshold: %d\n', i, numFaces, t);
-            if foundFeature
-                break;
-            end
-            
-            featDetector = vision.CascadeObjectDetector(feature, 'MergeThreshold', t, 'MinSize', [25, 25]);
-            features     = featDetector.step(faceImage);
-            numFeatures  = size(features, 1);
-
-            if numFeatures == 0
-                continue;
-            end
-            
-            for j=1:numFeatures
-                if  containsBBox(faces(i,:), features(j,:))
-                    s            = struct('Face', faces(i,:), feature, features(j,:));
-                    bboxStructs  = [bboxStructs ; s];
-                    foundFeature = true;
-                    break;
-                end
-            end
+    for threshold=hi:-1:lo
+        detector = vision.CascadeObjectDetector(feature_type, 'MergeThreshold', threshold);
+        bboxes   = detector.step(I);
+        if size(bboxes, 1) > 0
+            BBOX = bboxes;
+            break;
         end
     end
 end
 
-function [result] = containsBBox(outer, inner)
+%%
+%
+function [BBOX] = get_single_feature(I, feature_type, hi, lo)
+
+    BBOX = [];
+    
+    if nargin ~= 4
+        lo = 3;
+    end
+    if nargin ~= 3
+        hi = 15;
+    end
+    
+    for threshold=hi:-1:lo
+        detector = vision.CascadeObjectDetector(feature_type, 'MergeThreshold', threshold);
+        bboxes   = detector.step(I);
+        if size(bboxes, 1) == 1
+            BBOX = bboxes;
+            break;
+        end
+    end
+end
+
+%%
+%
+function [result] = contains_box(outer, inner)
     outX   = outer(1);
     outY   = outer(2);
     outW   = outer(3);
--- test_detect_faces.m
+++ test_detect_faces.m
@@ -1,16 +1,51 @@
 %%
 
+%im = im2double(imread('data/easy/iu.jpg'));
+%im = im2double(imread('data/easy/jennifer.jpg'));
+%im = im2double(imread('data/hard/jennifer_xmen.jpg'));
+%im = im2double(imread('data/hard/mj.jpg'));
+%im = im2double(imread('data/hard/0lliviaa.jpg'));
+%im = im2double(imread('data/hard/0b4e3684ebff3455f471bb82a0173f48.jpg'));
+%im = im2double(imread('data/hard/4b5d69173e608408ecf97df87563fd34.jpg'));
+im = im2double(imread('data/hard/14b999d49e77c6205a72ca87c2c2e5df.jpg'));
+%im = im2double(imread('data/hard/53e34a746d54adb574ab169d624ccd0a.jpg'));
+%im = im2double(imread('data/hard/69daf49a8beb63dc35bf65b4e408cde9.jpg'));
+%im = im2double(imread('data/hard/314eeaedbe5732558841972afdbaf32f.jpg'));
+%im = im2double(imread('data/hard/beard-champs4.jpg'));
+%im = im2double(imread('data/hard/star-trek-2009-sample-003.jpg'));
+
+%%
+
 addpath('final');
 data      = load('final/face_p146_small.mat');
 model     = data.model;
-im        = im2double(imread('data/hard/0lliviaa.jpg'));
 [X,Y,BOX] = detect_faces(im, model);
+bbox      = [BOX(1), BOX(2), BOX(3) - BOX(1), BOX(4) - BOX(2)];
 
 %%
 
 imshow(im);
 hold on;
 plot(X, Y, 'o', 'Color', 'g');
-
-bbox = [BOX(1), BOX(2), BOX(3) - BOX(1), BOX(4) - BOX(2)];
 rectangle('Position', bbox, 'EdgeColor', 'r', 'LineWidth', 2);
+hold off;
+
+%% 
+
+expand_by = 30;
+I = imcrop(im, [bbox(1:2) - (expand_by .* 0.5), bbox(3:4) + (expand_by .* 1)]);
+get_face_features(I);
+
+%%
+
+detector = vision.CascadeObjectDetector('Mouth', 'MergeThreshold', 6);
+bboxes   = detector.step(imsharpen(imadjust(rgb2gray(I))));
+
+imshow(I);
+hold on;
+for i=1:size(bboxes, 1)
+    % Show the face bounding box
+    rectangle('Position', bboxes(i,:), 'EdgeColor', 'r', 'LineWidth', 2);
+end
+hold off;
+
