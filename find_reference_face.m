7d6ce3c5963fd66dc253a0f79881d1e5975abd86
Mike Woods <mikeswoods@gmail.com>
2014-12-14 14:46:06 -0500

    Added reference faces

--- find_reference_face.m
+++ find_reference_face.m
@@ -4,8 +4,43 @@
 % model     = The detection model, as used by detect_faces()
 % target_im =
 % ref_image =
-function [ref_image] = find_reference_face(model, target_im)
+function [ref_image] = find_reference_face(model, target_im, threshold)
 
+    if nargin ~= 3
+        threshold = 0.2;
+    end
 
+    [X,Y,bbox,orientation] = detect_faces(target_im, model, threshold);
+
+    if orientation == -90
+        1
+    elseif orientation == -75
+        1
+    elseif orientation == -60
+        1
+    elseif orientation == -45
+        1
+    elseif orientation == -30
+        1
+    elseif orientation == -15
+        1
+    elseif orientation == 0
+        1
+    elseif orientation == 15
+        1
+     elseif orientation == 30
+        1
+    elseif orientation == 45
+        1
+    elseif orientation == 60
+        1
+    elseif orientation == 75
+        1
+    elseif orientation == 90
+        1
+    else
+        error('Non-quantized orientation: %f', orientation);
+    end
+    
 end
 
--- process_reference_images.m
+++ process_reference_images.m
@@ -6,6 +6,114 @@
 addpath('fitw_detect');
 data  = load('fitw_detect/face_p146_small.mat');
 model = data.model;
+threshold = 0.2;
 
 %%
+
+% -90
+fprintf(1, '-90\n');
+im_minus_90 = im2double(imread('data/reference/minus-90.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_minus_90, model, threshold);
+ref_minus_90 = struct('image', im_minus_90, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_minus_90; clear X; clear Y; clear bbox; clear orientation;
+
+% -75
+fprintf(1, '-75\n');
+im_minus_75 = im2double(imread('data/reference/minus-75.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_minus_75, model, threshold);
+ref_minus_75 = struct('image', im_minus_75, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_minus_75; clear X; clear Y; clear bbox; clear orientation;
+
+% -60
+fprintf(1, '-60\n');
+im_minus_60 = im2double(imread('data/reference/minus-60.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_minus_60, model, threshold);
+ref_minus_60 = struct('image', im_minus_60, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_minus_60; clear X; clear Y; clear bbox; clear orientation;
+
+% -45
+fprintf(1, '-45\n');
+im_minus_45 = im2double(imread('data/reference/minus-45.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_minus_45, model, threshold);
+ref_minus_45 = struct('image', im_minus_45, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_minus_45; clear X; clear Y; clear bbox; clear orientation;
+
+% -30
+fprintf(1, '-30\n');
+im_minus_30 = im2double(imread('data/reference/minus-30.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_minus_30, model, threshold);
+ref_minus_30 = struct('image', im_minus_30, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_minus_30; clear X; clear Y; clear bbox; clear orientation;
+
+% -15
+fprintf(1, '-15\n');
+im_minus_15 = im2double(imread('data/reference/minus-15.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_minus_15, model, threshold);
+ref_minus_15 = struct('image', im_minus_15, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_minus_15; clear X; clear Y; clear bbox; clear orientation;
+
+% 0
+fprintf(1, '0\n');
+im_zero = im2double(imread('data/reference/zero-degrees.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_zero, model, threshold);
+ref_zero = struct('image', im_zero, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_zero; clear X; clear Y; clear bbox; clear orientation;
+
+% +15
+fprintf(1, '+15\n');
+im_plus_15 = im2double(imread('data/reference/plus-15.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_plus_15, model, threshold);
+ref_plus_15 = struct('image', im_plus_15, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_plus_15, clear X; clear Y; clear bbox; clear orientation;
+
+% +30
+fprintf(1, '+30\n');
+im_plus_30 = im2double(imread('data/reference/plus-30.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_plus_30, model, threshold);
+ref_plus_30 = struct('image', im_plus_30, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_plus_30, clear X; clear Y; clear bbox; clear orientation;
+
+% +45
+fprintf(1, '+45\n');
+im_plus_45 = im2double(imread('data/reference/plus-45.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_plus_45, model, threshold);
+ref_plus_45 = struct('image', im_plus_45, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_plus_30, clear X; clear Y; clear bbox; clear orientation;
+
+% +60
+fprintf(1, '+60\n');
+im_plus_60 = im2double(imread('data/reference/plus-60.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_plus_60, model, threshold);
+ref_plus_60 = struct('image', im_plus_60, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_plus_60, clear X; clear Y; clear bbox; clear orientation;
+
+% +75
+fprintf(1, '+75\n');
+im_plus_75 = im2double(imread('data/reference/plus-75.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_plus_75, model, threshold);
+ref_plus_75 = struct('image', im_plus_75, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_plus_75, clear X; clear Y; clear bbox; clear orientation;
+
+% +90
+fprintf(1, '+90\n');
+im_plus_90 = im2double(imread('data/reference/plus-90.jpg'));
+[X,Y,bbox,orientation] = detect_faces(im_plus_90, model, threshold);
+ref_plus_90 = struct('image', im_plus_90, 'x', X, 'y', Y, 'bbox', bbox, 'orientation', orientation);
+clear im_plus_90, clear X; clear Y; clear bbox; clear orientation;
+
+%%
+save('reference_face.mat' ...
+    ,'ref_minus_90' ...
+    ,'ref_minus_75' ...
+    ,'ref_minus_60' ...
+    ,'ref_minus_45' ...
+    ,'ref_minus_30' ...
+    ,'ref_minus_15' ...
+    ,'ref_zero' ...
+    ,'ref_plus_15' ...
+    ,'ref_plus_30' ...
+    ,'ref_plus_45' ...
+    ,'ref_plus_60' ...
+    ,'ref_plus_75' ...
+    ,'ref_plus_90'); 
 
