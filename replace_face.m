9f4f460c91caf5345213096ecb72654c85d1a408
postmandev <michael.omeara@gmail.com>
2014-12-19 15:55:23 -0500

    Adding power point presentation and images

--- replace_face.m
+++ replace_face.m
@@ -23,6 +23,13 @@ function [im_out] = replace_face(target_im, max_faces)
     fprintf(1, '> Detecting face in target image...\n');
     [target_X, target_Y, target_bbox, target_orientation] = detect_faces(target_im, model);
     
+    
+    figure; imshow(target_im); hold on;
+    labels = cellstr( num2str([1:68]') );
+    plot(target_X, target_Y, 'rx')
+    text(target_X, target_Y, labels, 'VerticalAlignment','bottom', ...
+                             'HorizontalAlignment','right')
+    hold off;
     num_faces = numel(target_orientation);
     fprintf(1, '> Found %d faces', num_faces);
     
--- test.m
+++ test.m
@@ -7,8 +7,8 @@ basic = load('data/basicset.mat');
 %im = im2double(imread('data/testset/blending/Official_portrait_of_Barack_Obama.jpg'));
 % im = im2double(imread('data/testset/blending/060610-beard-championships-bend-stroomer-0002.jpg'));
 % im = im2double(imread('data/testset/blending/b1.jpg'));
-im = im2double(imread('data/testset/blending/bc.jpg'));
-% im = im2double(imread('data/testset/blending/Jennifer_lawrence_as_katniss-wide.jpg'));
+%im = im2double(imread('data/testset/blending/bc.jpg'));
+im = im2double(imread('data/testset/blending/Jennifer_lawrence_as_katniss-wide.jpg'));
 %im = im2double(imread('data/testset/blending/jennifer-lawrences-mystique-new-x-men-spin-off-movie.jpg'));
 %im = im2double(imread('data/testset/blending/Michael-Jordan.jpg'));
 %im = im2double(imread('data/testset/blending/Official_portrait_of_Barack_Obama.jpg'));
@@ -20,14 +20,14 @@ im = im2double(imread('data/testset/blending/bc.jpg'));
 % im = im2double(imread('data/testset/pose/p1.jpg'));
 % im = im2double(imread('data/testset/pose/p2.jpg'));
 %im = im2double(imread('data/testset/pose/Pepper-and-Tony-tony-stark-and-pepper-potts-9679158-1238-668.jpg'));
-%im = im2double(imread('data/testset/pose/robert-downey-jr-5a.jpg'));
-im = im2double(imread('data/testset/pose/star-trek-2009-sample-003.jpg'));
+im = im2double(imread('data/testset/pose/robert-downey-jr-5a.jpg'));
+%im = im2double(imread('data/testset/pose/star-trek-2009-sample-003.jpg'));
 
 %% === More set ===
 %im = im2double(imread('data/testset/more/real_madrid_2-wallpaper-960x600.jpg'));
 %im = im2double(imread('data/testset/more/marvels-the-avengers-wallpapers-01-700x466.jpg'));
-im = im2double(imread('data/testset/more/jkweddingdance-jill_and_kevin_wedding_party.jpg'));
-%im = im2double(imread('data/testset/more/burn-marvel-s-the-avengers.jpg'));
+%im = im2double(imread('data/testset/more/jkweddingdance-jill_and_kevin_wedding_party.jpg'));
+im = im2double(imread('data/testset/more/burn-marvel-s-the-avengers.jpg'));
 
 im = imresize(im, 2);
 
